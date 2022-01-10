3000c0cb9867b1e553f64e9435e79811449a646f
kshitijdhara <imkshitijdhara@gmail.com>
2022-01-10 22:47:24 +0530

    created dictionary of routes

--- app/vrp_demo.py
+++ app/vrp_demo.py
@@ -96,21 +96,27 @@ def print_solution(data, manager, routing, solution):
     """Prints solution on console."""
     print(f'Objective: {solution.ObjectiveValue()}')
     total_distance = 0
+    plan_dict = dict()
     for vehicle_id in range(data['num_vehicles']):
         index = routing.Start(vehicle_id)
         plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        plan_list = list()
         route_distance = 0
         while not routing.IsEnd(index):
             plan_output += ' {} -> '.format(manager.IndexToNode(index))
+            plan_list.append(manager.IndexToNode(index))
             previous_index = index
             index = solution.Value(routing.NextVar(index))
             route_distance += routing.GetArcCostForVehicle(
                 previous_index, index, vehicle_id)
         plan_output += '{}\n'.format(manager.IndexToNode(index))
+        plan_dict[vehicle_id] = plan_list
         plan_output += 'Distance of the route: {}m\n'.format(route_distance)
         print(plan_output)
         total_distance += route_distance
     print('Total Distance of all routes: {}m'.format(total_distance))
+    print('\n\n\n Plan dict ===> ')
+    print(plan_dict)
 
 
 def main():
