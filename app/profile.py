c41ca50b078efc1f6242f2043bed280224b2ee70
kshitijdhara <imkshitijdhara@gmail.com>
2022-01-10 18:17:43 +0530

    fixed profile

--- app/profile.py
+++ app/profile.py
@@ -1,30 +1,38 @@
+from pkg_resources import register_namespace_handler
 from app import application, vehicle_collection
 
-from flask import Flask, request
+from flask import Flask, request,jsonify
 
 
 
 # api to update profile using UID as document id
-@application.route('/updateprofile',methods=['POST'])
+@application.route('/updateprofile', methods=['POST'])
 def update_profile():
     if request.method == 'POST':
         try:
-            result = request.form()
+            result = request.form
             data = {
-                "uid" : result.get ("uid"),
-                "email" : result.get("email"),
-                "name" : result.get("name"),
-                "contact": result.get("contact"),
-                "address": result.get("address"),
-                "driver_license": result.get("dl"),
-                "vehicle_number" : result.get("vno"),
-                "vehicle_rc": result.get("vrc"),
-                "status": "away", # away or active
-
-
+                "uid": result.get ('uid'),
+                "email": result.get('email'),
+                "name": result.get('name'),
+                "contact": result.get('contact'),
+                "address": result.get('address'),
+                "driver_license": result.get('dl'),
+                "vehicle_number": result.get('vno'),
+                "vehicle_rc": result.get('vrc'),
+                "status": "away"
             }
+            print("Data => ")
+            print(data)
             uid = result.get("uid")
-            vehicle_collection.document(uid).update(data)
+            print("UID =>  " + uid)
+            vehicle_collection.document(uid).set(data)
+            response = {
+                "status": "Success",
+                "type": "Update Profile Success",
+                "msg": data
+                }
+            return response
 
         except Exception as e:
             response = {
@@ -36,16 +44,18 @@ def update_profile():
 
 
 # api to get the vehicle profile using UID as input 
-@application.route('/profile/<uid>',methods=['GET'])
+@application.route('/profile/<uid>', methods=['GET'])
 def get_profile(uid):
     if request.method == 'GET':
         try:
-            profile = vehicle_collection.document(id).get().to_dict()
+            profile = vehicle_collection.document(uid).get().to_dict()
+            print(profile)
             response = {
               "status": "Success",
               "type": "Get Profile Success",
               "msg": profile  
             }
+            return response
         except Exception as e:
             response = {
                 "status": "Failed",
